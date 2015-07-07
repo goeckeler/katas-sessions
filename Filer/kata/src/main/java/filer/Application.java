6ca6c6f458ca451f87705a69bab258c97b256468
goeckeler <thorsten@goeckeler.com>
2015-07-07 16:28:32 +0200

    Added runtime parameters complete with usage details.

--- Filer/kata/src/main/java/filer/Application.java
+++ Filer/kata/src/main/java/filer/Application.java
@@ -1,44 +1,66 @@
 package filer;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.ParserProperties;
 
 /**
  * File copy test driver
  */
 public class Application
 {
+  @Option(name = "-l", aliases = "--levels", metaVar = "DEPTH", usage = "copy folders till this level starting with 1")
+  private Integer depth = 1;
+
+  @Option(help = true, name = "-h", aliases = "--help", usage = "prints this usage details and stops")
+  private boolean showUsage = false;
+
+  @Option(name = "-a", aliases = "--algorithm", metaVar = "ALGORITHM",
+      usage = "define which implementation to use i.e. 'channel', 'files', 'reader'")
+  private String algorithm;
+
+  // receives other command line parameters than options
+  @Argument
+  private List<String> arguments = new ArrayList<String>();
+
   public static void main(final String[] args)
     throws IOException
   {
-//    CommandLine commandLine = Arguments.parse(args);
-//    
-//    if (commandLine.getArgs().length != 2) {
-//      System.out.println("Usage: java -jar <filer.jar> <sourceDirectory> <targetDirectory>");
-//      System.exit(1);
-//    }
+    new Application().runWith(args);
+  }
 
-    new FilerUtil().copy(args[0], args[1]);
+  private void runWith(final String[] args)
+    throws IOException
+  {
+    CmdLineParser parser = new CmdLineParser(this, ParserProperties.defaults().withUsageWidth(120));
+    try {
+      // parse the arguments.
+      parser.parseArgument(args);
+
+      if (arguments.isEmpty() || arguments.size() < 2 || showUsage) {
+        usage(parser);
+      }
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      System.err.println();
+      usage(parser);
+    }
+
+    new FilerUtil().copy(arguments.get(0), arguments.get(1));
+  }
+
+  private void usage(final CmdLineParser parser) {
+    System.err.println("Usage: java -jar <filer.jar> [-hla] <sourceDirectory> <targetDirectory>");
+    System.err.println();
+    parser.printUsage(System.err);
+    System.err.println();
+
+    System.exit(1);
   }
 }
-
-//class Arguments
-//{
-//  private static Options options = new Options();
-//
-//  static {
-//    options.addOption("i", "implementation", true,
-//        "Define which implementation to use i.e. 'channel', 'files', 'reader'");
-//  }
-//
-//  public static CommandLine parse(final String[] args) {
-//    CommandLine commandLine = null;
-//    try {
-//      CommandLineParser parser = new PosixParser();
-//      commandLine = parser.parse(options, args);
-//    } catch (ParseException exp) {
-//      System.out.println("Invalid options: " + exp.getMessage());
-//      return null;
-//    }
-//    return commandLine;
-//  }
-//}
